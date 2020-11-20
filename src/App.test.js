0c0676dab952e266550123920cec2585828f0d7e
juliusCho <johncrist2000@gmail.com>
2020-11-20 23:36:33 +0900

    Add Bootstrap styling

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme'
+import { findByTestAttr } from '../test/testUtils'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
+  const container = findByTestAttr(wrapper, 'App')
+  expect(container.length).toBe(1)
 });
