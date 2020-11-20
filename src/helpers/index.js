859ea36675d07729696692914558d639396c5d5e
juliusCho <johncrist2000@gmail.com>
2020-11-20 23:47:20 +0900

    Add getLetterMatchCount helper function and tests

---
+++ src/helpers/index.js
@@ -0,0 +1,11 @@
+/**
+ * @method getLetterMatchCount
+ * @param {string} guessedWord - Guessed word.
+ * @param {string} secretWord - Secret word.
+ * @returns {number} - Number of letters matched between guessed word and correct word
+ */
+export function getLetterMatchCount(guessedWord, secretWord) {
+  const secretLetterSet = new Set(secretWord.split(''))
+  const guessedWordSet = new Set(guessedWord.split(''))
+  return [...secretLetterSet].filter(letter => guessedWordSet.has(letter)).length
+}
